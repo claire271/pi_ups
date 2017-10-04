45e42df755e96a30512510585005b3865d44f3da
Tiger Huang <tigerrun1998@gmail.com>
2017-10-03 22:07:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,18 @@ cd gambezi_rpi-master
 cd ..
 rm -rf gambezi_rpi-master
 
+# Install gambezi-python
+wget https://github.com/tigerh/gambezi_python/archive/master.zip
+unzip master.zip
+rm master.zip
+mv gambezi_python-master gambezi_python
+
 # Install packages
+sudo apt-get install python3-pip
+sudo pip3 install wiringpi websocket-client
+
+# Enable I2C
+sudo bash -c ". raspi-config nonint; do_i2c 0"
 
 # Install init scripts
 sudo cp pi_ups.sh /etc/init.d/
